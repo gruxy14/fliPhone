f7db4bc32ab77e38c4e1c8142d785cfb6d5c0402
gruxy14 <grant.costa@duke.edu>
2015-11-18 17:24:13 -0500

    view controller set up

--- fliPhone/fliPhone/ViewController.swift
+++ fliPhone/fliPhone/ViewController.swift
@@ -11,17 +11,23 @@ import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var flips: UIButton!
+    @IBOutlet weak var currentScore: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        flips.setTitle("Click to Start", forState: UIControlState.Normal)
+        flips.backgroundColor = (UIColor.yellowColor())
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func beginFlip(sender: UIButton) {
-        var correct_flip = 0
-        var your_flip = 0
-        while correct_flip == your_flip{
-            correct_flip = Int((arc4random_uniform(20)+1)/2)
-            
+        var correctFlip = 0
+        var yourFlip = 0
+        while correctFlip == yourFlip{
+            correctFlip = Int((arc4random_uniform(9)+1))
+            flips.setTitle(String(correctFlip), forState: UIControlState.Normal)
+            flips.titleLabel!.font = UIFont(name: "System", size: 30)
+            //sleep(2)
         }
     }
     
